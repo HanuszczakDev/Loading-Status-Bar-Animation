c07c86f455cce047c0db53a5d9893724fae2e814
ahanuszczak <hanyhusajn@gmail.com>
2023-03-30 16:14:11 +0200

    layout content_main | cutomButton onClickListener implementation to start download

--- app/src/main/java/com/hanuszczak/loadingstatusbaranimation/MainActivity.kt
+++ app/src/main/java/com/hanuszczak/loadingstatusbaranimation/MainActivity.kt
@@ -10,36 +10,68 @@ import android.content.IntentFilter
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     private var downloadID: Long = 0
 
+    private lateinit var customButton: LoadingButton
+    private lateinit var radioGroup: RadioGroup
+    private lateinit var glideRadio: RadioButton
+    private lateinit var loadAppRadio: RadioButton
+    private lateinit var retrofitRadio: RadioButton
+
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
+
+    private var url: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+        customButton = findViewById(R.id.custom_button)
+
+        radioGroup = findViewById(R.id.radio_group)
+        glideRadio = findViewById(R.id.glide_radio)
+        loadAppRadio = findViewById(R.id.load_app_radio)
+        retrofitRadio = findViewById(R.id.retrofit_radio)
+
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
-        findViewById<LoadingButton>(R.id.custom_button).setOnClickListener {
-            download()
+        customButton.setOnClickListener {
+            if (radioGroup.checkedRadioButtonId == -1) {
+                Toast.makeText(
+                    this, getString(R.string.no_radio_group_selected),
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            } else {
+                if (glideRadio.isChecked) url = URL_GLIDE
+                if (loadAppRadio.isChecked) url = URL_UDACITY
+                if (retrofitRadio.isChecked) url = URL_RETROFIT
+                download(url)
+            }
         }
     }
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            Log.d("MainActivity", "downloadId: $id")
         }
     }
 
-    private fun download() {
+    private fun download(url: String?) {
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(Uri.parse(url))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
@@ -47,13 +79,13 @@ class MainActivity : AppCompatActivity() {
                 .setAllowedOverRoaming(true)
 
         val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-        downloadID =
-            downloadManager.enqueue(request)// enqueue puts the download request in the queue.
+        downloadID = downloadManager.enqueue(request)// enqueue puts the download request in the queue.
     }
 
     companion object {
-        private const val URL =
-            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val URL_UDACITY = "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val URL_GLIDE = "https://github.com/bumptech/glide/archive/master.zip"
+        private const val URL_RETROFIT = "https://github.com/square/retrofit/archive/master.zip"
         private const val CHANNEL_ID = "channelId"
     }
 }
