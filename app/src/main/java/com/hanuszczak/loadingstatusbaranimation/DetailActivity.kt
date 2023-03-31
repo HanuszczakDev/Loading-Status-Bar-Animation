0d9fc65745a5f426a389f6b9bbc833b961dd4d6a
ahanuszczak <hanyhusajn@gmail.com>
2023-03-31 15:20:38 +0200

    mockup layout with getStrinExtra implementation

--- app/src/main/java/com/hanuszczak/loadingstatusbaranimation/DetailActivity.kt
+++ app/src/main/java/com/hanuszczak/loadingstatusbaranimation/DetailActivity.kt
@@ -1,17 +1,32 @@
 package com.hanuszczak.loadingstatusbaranimation
 
+import android.app.NotificationManager
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var fileNameTextView: TextView
     private lateinit var statusTextView: TextView
+    private lateinit var confirmButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        fileNameTextView = findViewById(R.id.repository_name_text_view)
+        statusTextView = findViewById(R.id.status_text_view)
+        confirmButton = findViewById(R.id.confirm_button)
+
+        val notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.cancelAll()
+
+        fileNameTextView.text = intent.getStringExtra("downloadedUrl")
+        statusTextView.text = intent.getStringExtra("downloadStatus")
+
+        confirmButton.setOnClickListener { onBackPressedDispatcher.onBackPressed() }
     }
 
 }
